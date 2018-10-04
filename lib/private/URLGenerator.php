dc9266e83f1c6b1a3dc40acaa1f4221e03c8e6ed
Roeland Jago Douma <roeland@famdouma.nl>
2018-10-04 21:06:00 +0200

    Add function to generate urls for OCS routes
    fixes #11617
    
    The OCS routes are only absolute for now as they are often exposed to
    the outside anyway and are on a different endpoint than index.php in
    anyway.
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/private/URLGenerator.php
+++ lib/private/URLGenerator.php
@@ -91,6 +91,14 @@ class URLGenerator implements IURLGenerator {
 		return $this->getAbsoluteURL($this->linkToRoute($routeName, $arguments));
 	}
 
+	public function linkToOCSRouteAbsolute(string $routeName, array $arguments = []): string {
+		$route = \OC::$server->getRouter()->generate('ocs.'.$routeName, $arguments);
+		$route = substr($route, 7);
+		$route = '/ocs/v2.php' . $route;
+
+		return $this->getAbsoluteURL($route);
+	}
+
 	/**
 	 * Creates an url
 	 * @param string $app app
